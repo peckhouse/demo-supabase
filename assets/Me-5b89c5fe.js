0876131a584620a06ae8677daf9ec3509ccac1cc
peckhouse <millennionprandi@gmail.com>
2023-06-02 19:02:05 +0900

    Updates

---
+++ assets/Me-5b89c5fe.js
@@ -0,0 +1,1 @@
+import{d as s,a,j as t,o,c as n,b as r,t as c,k as l}from"./index-331ba977.js";const m={key:0},p=s({__name:"Me",setup(_){const{myUser:e}=a();return(i,u)=>t(e)?(o(),n("div",m,[r("div",null,"Hello "+c(t(e).email),1)])):l("",!0)}});export{p as default};
