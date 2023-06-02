54ad5949e74c4bdf571d9971e7fbe128515d321b
peckhouse <millennionprandi@gmail.com>
2023-06-02 18:47:28 +0900

    Updates

--- assets/EmailConfirmation-dfd28e89.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,o as n,c as o,b as e,t as s}from"./index-859b4b64.js";const a={},i=e("h1",{class:"text-3xl"},"Thanks for registering!",-1);function c(r,l){return n(),o("div",null,[i,e("p",null," Please confirm your email to finishing registering: "+s(r.$route.query.email),1)])}const _=t(a,[["render",c]]);export{_ as default};
